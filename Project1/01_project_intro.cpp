fa36a1383f08cc3006626b2bd5733b3efdc8b425
Tsygan <thuganenko@gmail.com>
2026-08-31 19:33:31 +0300

    Add project files.

---
+++ Project1/01_project_intro.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	cout << "I\n\tLove\n\t\tYou\n\t\t\tC++!\n\n\t...:::RESUME:::...\nName:\t\t\tIvan\nSurname:\t\tIvanov\nLast name:\t\tIvanovich\n\nDate of birth:\t\t22.06.06\nCity:\t\t\tRivne\nAge:\t\t\t17\n\nHobby:\t\t\tC++\n::...............................::\n\nEvery\n\thunter\n\t\twants\n\t\t\tto\n\t\t\t\tknow\n\t\t\t\t\tpheasant\n\t\t\t\t\t\tis.";
+}
